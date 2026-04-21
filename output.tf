ea349311d705180ea48753566940e971462f7008
Julio Hawthorne <julio@vltraheaven.io>
2026-04-21 13:42:47 -0700

    Removed vm/lb names from ip address outputs

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
 output "vm_ip_addresses" {
-  value      = [for vm in data.harvester_virtualmachine.vm : "${vm.name}: ${join(", ", vm.network_interface[*].ip_address)}"]
+  value      = [for vm in data.harvester_virtualmachine.vm : "${join(", ", vm.network_interface[*].ip_address)}"]
   depends_on = [harvester_virtualmachine.vm]
 }
 
 output "vm_lb_ip_address" {
-  value      = var.create_lb ? [for lb in data.harvester_loadbalancer.vm_lb : "${lb.name}: ${lb.ip_address}"] : null
+  value      = var.create_lb ? [for lb in data.harvester_loadbalancer.vm_lb : "${lb.ip_address}"] : null
 }
